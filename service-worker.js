d1c65c09b1e4a913a7ad6fb3a6c400ba0af67390
S.Mohamed Sameer <mohamedsameer.s.2007@gmail.com>
2025-04-21 17:41:14 +0530

    Add new 512x512 icon image for application

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,8 @@ const FILES_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
+  'icons/icon-192.png',
+  'icons/icon-512.png',
   '/styles.css',
   '/app.js'
 ];
